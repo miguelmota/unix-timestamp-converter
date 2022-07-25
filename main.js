ebe83abc388143c1a3ff3e8144f73f56992353e2
Miguel Mota <hello@miguelmota.com>
2022-07-25 08:03:27 -0700

    Add options

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ function printOutput (dt) {
   output += '<ul>'
   output += `<li>UTC Unix Timestamp: <code>${dt.toSeconds()}</code></li>`
   output += `<li>UTC RFC-1123: <code>${dt.toUTC().toHTTP()}</code></li>`
+  output += `<li>UTC RFC-2822: <code>${dt.toUTC().toRFC2822()}</code></li>`
   output += `<li>UTC ISO-8601: <code>${dt.toUTC().toISO()}</code></li>`
   output += `<li>UTC Relative: <code>${dt.toUTC().toRelative()}</code></li>`
   output += `<li>UTC Timezone: <code>${dt.toUTC().toFormat('ZZZZ')}</code></li>`
@@ -21,11 +22,13 @@ function printOutput (dt) {
   output += '<ul>'
   output += `<li>UTC Unix Timestamp Start of Day: <code>${dt.toUTC().startOf('day').toSeconds() >>> 0}</code></li>`
   output += `<li>UTC RFC-1123 Start of Day: <code>${dt.toUTC().startOf('day').toHTTP()}</code></li>`
+  output += `<li>UTC RFC-2822 Start of Day: <code>${dt.toUTC().startOf('day').toRFC2822()}</code></li>`
   output += `<li>UTC ISO-8601 Start of Day: <code>${dt.toUTC().startOf('day').toISO()}</code></li>`
   output += '</ul>'
   output += '<ul>'
   output += `<li>UTC Unix Timestamp End of Day: <code>${dt.toUTC().endOf('day').toSeconds() >>> 0}</code></li>`
   output += `<li>UTC RFC-1123 End of Day: <code>${dt.toUTC().endOf('day').toHTTP()}</code></li>`
+  output += `<li>UTC RFC-2822 End of Day: <code>${dt.toUTC().endOf('day').toRFC2822()}</code></li>`
   output += `<li>UTC ISO-8601 End of Day: <code>${dt.toUTC().endOf('day').toISO()}</code></li>`
   output += '</ul>'
   output += '<ul>'
@@ -34,6 +37,7 @@ function printOutput (dt) {
   output += '<ul>'
   output += `<li>Local Unix Timestamp: <code>${dt.toLocal().toSeconds()}</code></li>`
   output += `<li>Local RFC-1123: <code>${dt.toLocal().toHTTP()}</code></li>`
+  output += `<li>Local RFC-2822: <code>${dt.toLocal().toRFC2822()}</code></li>`
   output += `<li>Local ISO-8601: <code>${dt.toLocal().toISO()}</code></li>`
   output += `<li>Local Relative: <code>${dt.toLocal().toRelative()}</code></li>`
   output += `<li>Local Timezone: <code>${dt.toLocal().toFormat('ZZZZ')}</code></li>`
@@ -41,21 +45,36 @@ function printOutput (dt) {
   output += '<ul>'
   output += `<li>Local Unix Timestamp Start of Day: <code>${dt.toLocal().startOf('day').toSeconds() >>> 0}</code></li>`
   output += `<li>Local Unix RFC-1123 Start of Day: <code>${dt.toLocal().startOf('day').toHTTP()}</code></li>`
+  output += `<li>Local Unix RFC-2822 Start of Day: <code>${dt.toLocal().startOf('day').toRFC2822()}</code></li>`
   output += `<li>Local Unix ISO-8601 Start of Day: <code>${dt.toLocal().startOf('day').toISO()}</code></li>`
   output += '</ul>'
   output += '<ul>'
   output += `<li>Local Unix Timestamp End of Day: <code>${dt.toLocal().endOf('day').toSeconds() >>> 0}</code></li>`
   output += `<li>Local Unix RFC-1123 End of Day: <code>${dt.toLocal().endOf('day').toHTTP()}</code></li>`
+  output += `<li>Local Unix RFC-2822 End of Day: <code>${dt.toLocal().endOf('day').toRFC2822()}</code></li>`
   output += `<li>Local Unix ISO-8601 End of Day: <code>${dt.toLocal().endOf('day').toISO()}</code></li>`
   output += '</ul>'
   $output.innerHTML = output
 }
 
-function unixToOutput () {
+function optionToOutput () {
   try {
     $output.textContent = ''
-    const timestamp = Math.floor(Number($input.value || Date.now() / 1000))
-    const dt = luxon.DateTime.fromSeconds(timestamp)
+    const value = $input.value
+    const $option = document.querySelector('input[name=option]:checked')
+    const option = $option.value
+    let dt
+    if (option === 'UnixTimestamp') {
+      const timestamp = Math.floor(Number(value || Date.now() / 1000))
+      dt = luxon.DateTime.fromSeconds(timestamp)
+    } else if (option === 'RFC-1123') {
+      console.log('hyo')
+      dt = luxon.DateTime.fromHTTP(value)
+    } else if (option === 'RFC-2822') {
+      dt = luxon.DateTime.fromRFC2822(value)
+    } else if (option === 'ISO-8601') {
+      dt = luxon.DateTime.fromISO(value)
+    }
     printOutput(dt)
   } catch (err) {
     alert(err.message)
@@ -74,7 +93,7 @@ function dateToOutput ({ year, month, day }) {
 
 $form1.addEventListener('submit', (event) => {
   event.preventDefault()
-  unixToOutput()
+  optionToOutput()
 })
 
 $form2.addEventListener('submit', (event) => {
@@ -89,4 +108,4 @@ $form2.addEventListener('submit', (event) => {
   })
 })
 
-unixToOutput()
+optionToOutput()
